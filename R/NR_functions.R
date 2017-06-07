ae20a4cf92829628196a0695427f93fe57521287
Till Halbach <halbach@nr.no>
2017-06-07 10:08:28 +0200

    Roxygen format for documentation

--- R/NR_functions.R
+++ R/NR_functions.R
@@ -8,23 +8,18 @@ require(lumiHumanIDMapping)
 require(genefilter)
 
 
-## =======================================================================================
-## mapToGenes
-## - Map from probes to genes
-## - Input: 
-##     data matrix where colnames(data)=sample IDs and rownames(data) = probe IDs
-## - Return value: 
-##     data matrix where colnames(data)=sample IDs and rownames(data) = gene IDs
-## =======================================================================================
-
+#' Map probes to genes.
+#' @export
+#' @param matrix where colnames(data)=sample IDs and rownames(data) = probe IDs
+#' @return matrix where colnames(data)=sample IDs and rownames(data) = gene IDs
 mapToGenes <- function(data) {
-  ## --- Map to gene symbol
+  # --- Map to gene symbol
   nuIDs <- rownames(data)
   mappingInfo <- nuID2RefSeqID(nuIDs, lib.mapping='lumiHumanIDMapping', returnAllInfo=TRUE)
   geneName <- as.character(mappingInfo[,3])
   geneName <- geneName[geneName!=""] 
   
-  ## --- Compute mean of probes for each gene
+  # --- Compute mean of probes for each gene
   exprs0 <- data
   uGeneNames <- unique(geneName)
   exprs <- matrix(NA, ncol=ncol(exprs0), nrow=length(uGeneNames))
@@ -37,33 +32,26 @@ mapToGenes <- function(data) {
 }
 
 
-## =======================================================================================
-##
-## performBackgroundCorrection
-## - Perform background correction and remove bad probes
-## - Input: 
-##     lumi object data where colnames(data)=sample IDs and rownames(data) = probe IDs
-##     exprs data matrix where rownames(exprs)=sample IDs and colnames(exprs) = probe IDs
-##     negCtrl matrix where rownames(exprs)=sample IDs and colnames(data) = negative control IDs
-## - Return value: 
-##     background corrected lumi object data where colnames(data)=sample IDs and rownames(data) = gene IDs
-##
-## =======================================================================================
-
+#' Perform background correction and remove bad probes.
+#' @export
+#' @param lumi object where colnames(data)=sample IDs and rownames(data) = probe IDs
+#' @param matrix where rownames(exprs)=sample IDs and colnames(exprs) = probe IDs
+#' @param matrix where rownames(exprs)=sample IDs and colnames(data) = negative control IDs
+#' @return background corrected lumi object where colnames(data)=sample IDs and rownames(data) = gene IDs
 performBackgroundCorrection <- function(data, exprs, negCtrl) {
   
-  ## --- Combine data, status vector (stating for each row if it corresponds to gene or control)
-  totalData <- t(cbind(exprs,negCtrl)) ## neg control probes
+  # --- Combine data, status vector (stating for each row if it corresponds to gene or control)
+  totalData <- t(cbind(exprs,negCtrl)) # neg control probes
   status <- c(rep("regular", ncol(exprs)), rep("negative", ncol(negCtrl)))
   
-  ## --- Background correct the probes using the limma nec function
-  data.nec <- nec(totalData, status)  ## this gives same result as adding detection.p.
+  # --- Background correct the probes using the limma nec function
+  data.nec <- nec(totalData, status)  # this gives same result as adding detection.p.
   
-  ## --- Remove the negative probes from the matrix: exprs is not log2 transformed
-  exprs <- t(data.nec)[,1:ncol(exprs)] ## remove the values of the negative controls 
-  exprs(data) <- t(exprs) ## Make sure exprs of data object is background corrected
+  # --- Remove the negative probes from the matrix: exprs is not log2 transformed
+  exprs <- t(data.nec)[,1:ncol(exprs)] # remove the values of the negative controls 
+  exprs(data) <- t(exprs) # Make sure exprs of data object is background corrected
   
-  ## --- Get rid of bad probes
+  # --- Get rid of bad probes
   probes <- nuID2IlluminaID(as.character(featureNames(data)), lib.mapping=NULL, species ="Human", idType='Probe')
   probe.quality <- unlist(mget(as.character(probes), illuminaHumanv3PROBEQUALITY, ifnotfound=NA))
   table(probe.quality, exclude=NULL) # check mapping and missing
@@ -77,19 +65,12 @@ performBackgroundCorrection <- function(data, exprs, negCtrl) {
 }
 
 
-## =======================================================================================
-##
-## Filter data 
-## - Filtering on pValue and presentLimit
-## - Input: 
-##     lumi object data where colnames(data)=sample IDs and rownames(data) = probe IDs
-##     p-value limit pValue
-##     presentLimit
-## - Return value: 
-##     filtered lumi object data where colnames(data)=sample IDs and rownames(data) = probe IDs
-##
-## =======================================================================================
-
+#' Filtering based on on pValue and presentLimit.
+#' @export
+#' @param lumi object where colnames(data)=sample IDs and rownames(data) = probe IDs
+#' @param p-value limit pValue
+#' @param presentLimit
+#' @return filtered lumi object where colnames(data)=sample IDs and rownames(data) = probe IDs
 filterData <- function(data, pValue, presentLimit) {
   presentcall <- detectionCall(data,Th=pValue)
   filterP <- which(presentcall>(presentLimit*ncol(data)))
@@ -99,17 +80,10 @@ filterData <- function(data, pValue, presentLimit) {
 }
 
 
-## =======================================================================================
-##
-## normalize data 
-## - Normalization procedure from Vanesssa
-## - Input: 
-##     lumi object data where colnames(data)=sample IDs and rownames(data) = probe IDs
-## - Return value: 
-##     normalized data matrix where colnames(data)=sample IDs and rownames(data) = probe IDs
-##
-## =======================================================================================
-
+#' Normalization procedure from Vanesssa.
+#' @export
+#' @param lumi object where colnames(data)=sample IDs and rownames(data) = probe IDs
+#' @return normalized matrix where colnames(data)=sample IDs and rownames(data) = probe IDs
 normalizeData <- function(data.new) {
   vstdata <- lumiT(data.new,method="vst")
   Nvstdata <- lumiN(vstdata,method="quantile")
